718382ef8786dd1a15123adc1518769ed4ec71ee
francoapesteguia <franco.apesteguia1@gmail.com>
2022-12-01 20:26:37 -0300

    a

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let num1 =parseInt (prompt("Ingresa un numero"));
+let num2 = parseInt(prompt("Ingresa un numero"));
+let resultado;
+resultado = num1 + num2
+alert("El resultado de la suma entre " + num1 + ' y ' + num2 + " es: " + resultado);
